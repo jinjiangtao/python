882b1b1079fa9b8693722927d1a5ca5bfc448417
jinjiangtao <840129243@qq.com>
2017-07-23 12:06:42 +0800

    add recommen

--- recommend/recommendations.py
+++ recommend/recommendations.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from math import sqrt 
 
 critics = {'Lisa Rose':{'Lady in the Water':2.5, 'Snakes On a Plane':3.5, 'Just My Luck':3.0, 'Superman Returns':3.5, 'You, Me and Dupree':2.5, 'The Night Listenner':3.0},
-'Gen Seymour':{'Lady in the Water':3.0, 'Snakes On a Plane':3.5, 'Just My Luck':1.5, 'Superman Returns':5.0, 'You, Me and Dupree':3.0, 'The Night Listenner':3.5},
+'Gene Seymour':{'Lady in the Water':3.0, 'Snakes On a Plane':3.5, 'Just My Luck':1.5, 'Superman Returns':5.0, 'You, Me and Dupree':3.0, 'The Night Listenner':3.5},
 'Michael Phillips':{'Lady in the Water':2.5, 'Snakes On a Plane':3.0,  'Superman Returns':3.5, 'The Night Listenner':4.0},
 'Claudia Puig':{'Snakes On a Plane':3.5, 'Just My Luck':3.0, 'Superman Returns':4.0, 'The Night Listenner':4.5},
 'Mick LaSalle':{'Lady in the Water':3.0, 'Snakes On a Plane':4.0, 'Just My Luck':2.0, 'Superman Returns':3.0, 'You, Me and Dupree':2.0, 'The Night Listenner':3.0},
@@ -10,7 +11,38 @@ critics = {'Lisa Rose':{'Lady in the Water':2.5, 'Snakes On a Plane':3.5, 'Just 
 'Toby':{'Snakes On a Plane':4.5, 'Superman Returns':4.0, 'You, Me and Dupree':1.0 }
 }
 
+def sim_distance(prefs, person1, person2):
+    si = {}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item] = 1
 
+    if len(si) == 0:return 0
+
+    sum_of_squares = sum([pow(prefs[person1][item] -prefs[person2][item], 2) for item in prefs[person1] if item in prefs[person2]])
+    
+    return 1/(1 +sqrt(sum_of_squares))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
