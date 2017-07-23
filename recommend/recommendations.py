f5259c2924f053218c72e074955adb8643f038b7
jinjiangtao <840129243@qq.com>
2017-07-23 10:46:23 +0800

    add recommend

---
+++ recommend/recommendations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+critics = {'Lisa Rose':{'Lady in the Water':2.5, 'Snakes On a Plane':3.5, 'Just My Luck':3.0, 'Superman Returns':3.5, 'You, Me and Dupree':2.5, 'The Night Listenner':3.0},
+'Gen Seymour':{'Lady in the Water':3.0, 'Snakes On a Plane':3.5, 'Just My Luck':1.5, 'Superman Returns':5.0, 'You, Me and Dupree':3.0, 'The Night Listenner':3.5},
+'Michael Phillips':{'Lady in the Water':2.5, 'Snakes On a Plane':3.0,  'Superman Returns':3.5, 'The Night Listenner':4.0},
+'Claudia Puig':{'Snakes On a Plane':3.5, 'Just My Luck':3.0, 'Superman Returns':4.0, 'The Night Listenner':4.5},
+'Mick LaSalle':{'Lady in the Water':3.0, 'Snakes On a Plane':4.0, 'Just My Luck':2.0, 'Superman Returns':3.0, 'You, Me and Dupree':2.0, 'The Night Listenner':3.0},
+'Jack Matthews':{'Lady in the Water':3.0, 'Snakes On a Plane':4.0, 'Just My Luck':3.0, 'Superman Returns':5.0, 'You, Me and Dupree':2.0, 'The Night Listenner':3.0},
+'Toby':{'Snakes On a Plane':4.5, 'Superman Returns':4.0, 'You, Me and Dupree':1.0 }
+}
+
+
+
+
+
